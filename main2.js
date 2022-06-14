8c579b3c66907e181924a2bfd2719ee924470428
lan986 <=lanaoms6@gmail.com>
2022-06-14 20:23:23 +0300

    updating file

--- main2.js
+++ main2.js
@@ -1,50 +1,44 @@
 /*TO GET THE VALUE OF INPUT USING ID */
+
 var customerNameInput = document.getElementById("customerName");
 var addressInput = document.getElementById("address");
 var cityInput = document.getElementById("city");
 var phoneInput = document.getElementById("phone");
 var btn_click = document.getElementById("click");
-var btn_clear=document.getElementById("clear")
-
+var btn_clear = document.getElementById("clear");
 var data = document.getElementById("data");
 var inputs = document.getElementsByClassName("inputs");
-console.log(inputs)
-var current_index=0;
+console.log(inputs);
+var current_index = 0;
+var namealert=document.getElementById("namealert")
+var array_customer=[]
+//console.log(localStorage.getItem("customers"));
 
-console.log((localStorage.getItem("customers")))
+//console.log(localStorage.getItem("customers"));
 
-console.log((localStorage.getItem("customers")))
 /* to display local storge which is store  when we relode the page */
-if(localStorage.getItem("customer_data")==null){
-    var array_customer = [];
-   
-   
-}else{
-         array_customer=JSON.parse(localStorage.getItem("customers"))
-
-    displayData();
+if (localStorage.getItem("EmployeesList") == null ){
+  var array_customer = [] ; 
+}else {
+  array_customer = JSON.parse(localStorage.getItem("EmployeesList"));
+  displayData() ; 
 }
+
 
 
 btn_click.onclick = function () {
-  if(btn_click.innerHTML=="Add"){
-
-  
-  addCustomer();
- 
-}else{
-  updateData();
-  
-}
-displayData();
-clear();
-
+  if (btn_click.innerHTML == "Add") {
+    addCustomer();
+  } else {
+    updateData();
+  }
+  displayData();
+  clear();
 };
 
-
 btn_clear.onclick = function () {
-  clear()
-}
+  clear();
+};
 /*function for add customer */
 function addCustomer() {
   var customer = {
@@ -55,11 +49,12 @@ function addCustomer() {
     phone: phoneInput.value,
   };
   array_customer.push(customer);
-  
-  localStorage.setItem("customers ",JSON.stringify(array_customer))
-  var x=localStorage.getItem("customers")
-  console.log(x)
-
+  console.log(array_customer)
+  localStorage.setItem("EmployeesList", JSON.stringify(array_customer));
+ 
+  //var x = localStorage.getItem("customer_data");
+  //console.log(x);
+  //console.log(array_customer)
 }
 
 /*function for display the data in table */
@@ -88,7 +83,6 @@ function displayData() {
   data.innerHTML = result;
 }
 
-
 /*function for clear the inputs values */
 function clear() {
   for (var i = 0; i < inputs.length; i++) {
@@ -100,26 +94,49 @@ function clear() {
 addressInput.value=""
 phoneInput.value=""*/
 }
- 
+
+
+
+
+
+deleteBtn.onclick = function(){
+  localStorage.removeItem("EmployeesList");
+  array_customer = [] ; 
+  data.innerHTML = "" ; 
+}
+
 /* function for the delete button in the table */
 function deleteCourse(index) {
-  console.log(index);
-  array_customer.splice(index, 1);
-  displayData();
+ 
+ 
+  swal({
+    title: "Are you sure?",
+    text: "Once deleted, you will not be able to recover this imaginary file!",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      array_customer.splice(index, 1);
+      localStorage.setItem("EmployeesList", JSON.stringify(array_customer));
+      displayData();
+      swal("Poof! has been deleted!", {
+        icon: "success",
+      });
+    } else {
+      swal("Your imaginary file is safe!");
+    }
+  });
 }
 //localStorage.setItem("test","thsis is test value")
 
-
-function search(val){
-  
-
+function search(val) {
   var result = " ";
 
-  for (var i = 0; i < array_customer.length; i++){  
-  if(array_customer[i].name.toLowerCase().includes(val.toLowerCase())){
-    
-  
-    result += `<td>${i}</td>
+  for (var i = 0; i < array_customer.length; i++) {
+    if (array_customer[i].name.toLowerCase().includes(val.toLowerCase())) {
+      result += `<td>${i}</td>
    <td>${array_customer[i].name}
    </td>
    <td>${array_customer[i].address}
@@ -134,25 +151,25 @@ function search(val){
    <td><a href='#editEmployeeModal' class='edit' onclick="deleteCourse(${i}) "><i class='material-icons'  title='Delete'>&#xE872;</i></a>
    </td></tr>`;
 
-  //  console.log(result) "</tr>"
-  //  console.log(result)
-  }}
+      //  console.log(result) "</tr>"
+      //  console.log(result)
+    }
+  }
   data.innerHTML = result;
 }
-
-function getCourseData(index){
-  console.log(index)
-  var customer=array_customer[index]
-  console.log(customer)
-   customerNameInput.value=customer.name
-   addressInput.value=customer.address
-   cityInput.value=customer.city
-   phoneInput.value=customer.phone
-  btn_click.innerHTML="update"
-  current_index=index
+/* ACTION HAPPENED WHEN WE CLICK UPDATE  BUTTON  */
+function getCourseData(index) {
+  console.log(index);
+  var customer = array_customer[index];
+  console.log(customer);
+  customerNameInput.value = customer.name;
+  addressInput.value = customer.address;
+  cityInput.value = customer.city;
+  phoneInput.value = customer.phone;
+  btn_click.innerHTML = "update";
+  current_index = index;
 }
-function updateData(){
-
+function updateData() {
   var customer = {
     // object for storing the values of inputs
     name: customerNameInput.value,
@@ -160,13 +177,33 @@ function updateData(){
     city: cityInput.value,
     phone: phoneInput.value,
   };
-  //هون عشان يعدل على ال row  اللي ضغطت عليه 
-  array_customer[current_index].name=customer.name
-  array_customer[current_index].address=customer.address
-  array_customer[current_index].city=customer.city
-  array_customer[current_index].phone=customer.phone
-  console.log(array_customer)
-  localStorage.setItem("course_list",JSON.stringify(array_customer))
+  //هون عشان يعدل على ال row  اللي ضغطت عليه
+  array_customer[current_index].name = customer.name;
+  array_customer[current_index].address = customer.address;
+  array_customer[current_index].city = customer.city;
+  array_customer[current_index].phone = customer.phone;
+  //console.log(array_customer);
+  localStorage.setItem("customer_data", JSON.stringify(array_customer));
+  swal("Good job!", "You clicked the button!", "success");
+  btn_click.innerHTML = "Add";
+}
 
-  btn_click.innerHTML="Add"
+
+customerName.onkeyup=function(){
+  var namepattern=/^[a-z]{2,10}$/
+  if(namepattern.test(customerName.value)){
+    //console.log((namepattern.test(customerName.value)))
+    btn_click.removeAttribute("disabled")
+    customerName.classList.add("valid")
+    customerName.classList.remove("is-invalid")
+    namealert.classList.add("d-none")
+  }
+  else{
+    btn_click.setAttribute("disabled",true)
+    customerName.classList.add("invalid")
+    customerName.classList.remove("valid")
+    //console.log(namealert.value)
+    namealert.classList.remove("d-none")
+
+  }
 }
